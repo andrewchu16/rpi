929684419733179a610ee0c43b581e43ef45b234
Andrew Chu <andrew.chu206@gmail.com>
2025-08-26 00:00:51 -0400

    Add logging

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,13 @@
+import json
+import logging
+import logging.config
+import atexit
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .auth.router import router as auth_router
 from .health.router import router as health_router
 from .upload.router import router as upload_router
+from logging.handlers import QueueHandler
 
 app = FastAPI()
 
@@ -15,6 +20,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 """
+
+# Setup logging at module level
+def setup_logging():
+    with open("./logging_config.json", "r") as f:
+        logging.config.dictConfig(json.load(f))
+    queue_handler: QueueHandler = logging.getHandlerByName("queue_handler")
+    
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
+
+# Initialize logging
+setup_logging()
+logger = logging.getLogger(__name__)
+logger.info("Starting the application")
 
 app.include_router(auth_router)
 app.include_router(health_router)
