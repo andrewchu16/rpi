b93cc785899ed7e188ab9b68991406f9369264e6
Andrew Chu <andrew.chu206@gmail.com>
2025-09-07 17:22:35 -0400

    Add llama.cpp servers

--- backend/src/chat/config.py
+++ backend/src/chat/config.py
@@ -8,10 +8,15 @@ class ChatConfig(BaseSettings):
     max_context_token_count: int = 500
     max_response_token_count: int = 300
 
-    llm_model_name: str = "./models/Llama-3.2-1B-Instruct-Q4_K_S.gguf"
-    embedding_model_name: str = "./models/embeddinggemma-300M-BF16.gguf"
 
     vector_store_path: str = "./stores/vector_store"
+    
+    # LLM settings
+    llama_cpp_llm_server_url: str = "http://llama_cpp_llm_server:8000"
+    llm_model_name: str = "./models/Llama-3.2-1B-Instruct-Q4_K_S.gguf"
+    
+    llama_cpp_embedding_server_url: str = "http://llama_cpp_embedding_server:8000"
+    embedding_model_name: str = "./models/embeddinggemma-300M-BF16.gguf"
 
 
 chat_config = ChatConfig()
